791046e022b2a88c366c1fcbb285c0d9e0ca7324
CarL021D <tcarl771@gmail.com>
2024-07-18 02:33:32 +0100

    project cleaned

--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -20,44 +20,6 @@ void check_args(int ac) {
 	// TODO bonus: options to set here later
 }
 
-bool cheksums_compar(t_icmp_pckt *sent_pckt, t_icmp_pckt *rcvd_pckt) {
-	
-	uint16_t rcvd_checksum;
-
-	rcvd_pckt->hdr.checksum = 0;
-	checksum(rcvd_pckt, sizeof(struct icmphdr) + PAYLOAD_SIZE);
-	rcvd_checksum = checksum(rcvd_pckt, sizeof(struct icmphdr) + PAYLOAD_SIZE);
-
-	// printf("Sent payload ------>   %s\n", sent_pckt->payload);
-	// printf("Received payload  ------>   %s\n", rcvd_pckt->payload);
-	// printf("Sent ICMP checksum ------>   %d\n", sent_pckt->hdr.checksum);
-	// printf("Received ICMP checksum ------>   %d\n", rcvd_checksum);
-
-	if (sent_pckt->hdr.checksum == rcvd_checksum - 8)
-		return true;
-	return false;
-}
-
-void print_rcvd_packet_response(t_data *data, char *buffer, t_icmp_pckt *pckt, uint16_t sequence, long double rtt_msec) {
-
-	t_icmp_pckt rcvd_pckt;
-	struct iphdr *ip_hdr = (struct iphdr *)buffer;
-	struct icmphdr *icmp_hdr = (struct icmphdr *)(buffer + (ip_hdr->ihl * 4));
-
-	memcpy(&rcvd_pckt.hdr, icmp_hdr, sizeof(struct icmphdr));
-	memcpy(rcvd_pckt.payload, buffer + (ip_hdr->ihl * 4) + sizeof(struct icmphdr), PAYLOAD_SIZE);
-
-	if (!cheksums_compar(pckt, &rcvd_pckt) && strcmp(data->ip_addr, "127.0.0.1"))
-		printf("CORRUPTED PAYLOAD\n");
-
-	if (icmp_hdr->type == ICMP_ECHOREPLY && icmp_hdr->un.echo.id == getpid()) {
-		sequence++;
-		uint16_t actual_seq = strcmp(data->ip_addr, "127.0.0.1") ? (sequence / 2) : sequence;
-		printf("64 bytes from %s: icmp_seq=%d ttl=%d time=%.3Lf ms\n", data->ip_addr, actual_seq, ip_hdr->ttl, rtt_msec);
-	}
-}
-
-
 void ping(t_data *data, struct sockaddr_in *addr_con) {
 	t_icmp_pckt pckt;
 	struct timespec time_start, time_end;
--- srcs/packet_managment.c
+++ srcs/packet_managment.c
@@ -1,20 +1,40 @@
 #include "../includes/ft_ping.h"
 
-void send_packet(t_data *data, t_icmp_pckt *pckt, struct sockaddr_in *addr_con) {
+bool cheksums_compar(t_icmp_pckt *sent_pckt, t_icmp_pckt *rcvd_pckt) {
+	
+	uint16_t rcvd_checksum;
 
-    if (sendto(data->sockfd, pckt, sizeof(t_icmp_pckt), 0, (struct sockaddr *)addr_con, sizeof(*addr_con)) <= 0) {
-        perror("sendto");
-        exit(EXIT_FAILURE);
-    }
+	rcvd_pckt->hdr.checksum = 0;
+	checksum(rcvd_pckt, sizeof(struct icmphdr) + PAYLOAD_SIZE);
+	rcvd_checksum = checksum(rcvd_pckt, sizeof(struct icmphdr) + PAYLOAD_SIZE);
+
+	// printf("Sent payload ------>   %s\n", sent_pckt->payload);
+	// printf("Received payload  ------>   %s\n", rcvd_pckt->payload);
+	// printf("Sent ICMP checksum ------>   %d\n", sent_pckt->hdr.checksum);
+	// printf("Received ICMP checksum ------>   %d\n", rcvd_checksum);
+
+	if (sent_pckt->hdr.checksum == rcvd_checksum - 8)
+		return true;
+	return false;
 }
 
-void receive_packet(t_data *data, char *buffer, struct sockaddr_in *addr_con) {
+void print_rcvd_packet_response(t_data *data, char *buffer, t_icmp_pckt *pckt, uint16_t sequence, long double rtt_msec) {
 
-	socklen_t addr_len = sizeof(*addr_con);
+	t_icmp_pckt rcvd_pckt;
+	struct iphdr *ip_hdr = (struct iphdr *)buffer;
+	struct icmphdr *icmp_hdr = (struct icmphdr *)(buffer + (ip_hdr->ihl * 4));
 
-    if (recvfrom(data->sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)addr_con, &addr_len) <= 0) {
-		perror("recvfrom");
-		exit(EXIT_FAILURE);
-	} 
+	memcpy(&rcvd_pckt.hdr, icmp_hdr, sizeof(struct icmphdr));
+	memcpy(rcvd_pckt.payload, buffer + (ip_hdr->ihl * 4) + sizeof(struct icmphdr), PAYLOAD_SIZE);
+
+	if (!cheksums_compar(pckt, &rcvd_pckt) && strcmp(data->ip_addr, "127.0.0.1"))
+		printf("CORRUPTED PAYLOAD\n");
+
+	if (icmp_hdr->type == ICMP_ECHOREPLY && icmp_hdr->un.echo.id == getpid()) {
+		sequence++;
+		uint16_t actual_seq = strcmp(data->ip_addr, "127.0.0.1") ? (sequence / 2) : sequence;
+		printf("64 bytes from %s: icmp_seq=%d ttl=%d time=%.3Lf ms\n", data->ip_addr, actual_seq, ip_hdr->ttl, rtt_msec);
+	}
+
+	//  Todo code and type error check to add here
 }
-void print_packet_diagnostic() {}
